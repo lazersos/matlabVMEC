508bcfeb78dffb436fee8426b034eb3e37fea6d2
Samuel Lazerson <lazersos@gmail.com>
2020-04-03 10:16:16 +0200

    Added option for user specified equilID.

--- STELLOPT/stellopt2neotransp.m
+++ STELLOPT/stellopt2neotransp.m
@@ -1,15 +1,40 @@
-function stellopt2neotransp(ext)
+function stellopt2neotransp(ext,varargin)
 %STELLOPT2NEOTRASP Calculates radial electric field using NEOTRANSP
 %   The STELLOPT2NEOTRANSP subroutine uses the NEOTRANSP library to
 %   calculate the radial electric field from a STELLOPT reconstruction run.
 %   The input extension of a run is passed to the routine and the 'tprof'
-%   and 'wout' files are read.
+%   and 'wout' files are read.  The options are as follows:
+%       list:       List all available equilID's
+%       equilid:    Specify specific equilID (w7x-sc1 is default)
 %
 %   Example
 %       stellopt2neotransp('test');
 %
 % Maintained by: Samuel Lazerson (sameul.lazerson@ipp.mpg.de)
 % Version:       1.00
+
+
+equilID='w7x-sc1'; % Standard
+
+% Handle varargin
+if nargin > 1
+    i=1;
+    while i < nargin
+        switch varargin{i}
+            case{'list','LIST'}
+                database=w7xdatabase();
+                list(database)
+                return;
+            case{'equilid','equilID','EQUILID'}
+                i=i+1;
+                equilID=varargin{i};
+            otherwise
+                disp(['Unrecognized Option: ' varargin{i}]);
+                return
+        end
+        i = i + 1;
+    end
+end
 
 % Get data
 tprof=importdata(['tprof.' ext]);
@@ -30,12 +55,6 @@ end
     
 B00axis= vmec_data.b0;
 phiedge = vmec_data.phi(end);
-
-% this will come later
-% database=w7xdatabase();
-% list(database)
-%equilID='w7x-hm1'; % High Mirror
-equilID='w7x-sc1'; % Standard
 
 rho = sqrt(tprof(:,1));
 ne  = tprof(:,2).*1E-20;
@@ -75,34 +94,42 @@ Opt.roots='i&e';          %can be 'i', 'e', 'i&e'
 Transp = neotransp(equilID,rho2,Prof,B00axis,Opt);
 
 % Now we need to fit the data
-dphi1 = Transp.dPhidskV(1,:);
-dphi2 = Transp.dPhidskV(2,:);
+factor = 1E-3;
+dphi1 = Transp.dPhidskV(1,:).*1E3;
+dphi2 = Transp.dPhidskV(2,:).*1E3;
+
+% Handle single root
+dphi1(isnan(dphi1)) = dphi2(isnan(dphi1));
+dphi2(isnan(dphi2)) = dphi1(isnan(dphi2));
+
 rho  = Transp.rho;
 s    = rho.*rho;
 fig = figure('Position',[ 1 1 1024 768],'Color','white');
 subplot(1,2,1);
-plot(s,dphi1./phiedge,'ob');
+plot(s,factor.*dphi1./phiedge,'ob');
 hold on;
-plot(s,dphi2./phiedge,'or');
+plot(s,factor.*dphi2./phiedge,'or');
 xlim([0 1]);
 set(gca,'FontSize',24);
 xlabel('Norm. Toridal Flux (s)');
 ylabel('dV/ds [kV/Wb]');
 title('NEOTRANSP E');
-dphi1(isnan(dphi1)) = dphi2(isnan(dphi1));
-dphi2(isnan(dphi2)) = dphi1(isnan(dphi2));
 dphim=0.5*(dphi1+dphi2);
-plot(s,dphim./phiedge,'ok');
+s = s(~isnan(dphim));
+dphi1 = dphi1(~isnan(dphim));
+dphi2 = dphi2(~isnan(dphim));
+dphim = dphim(~isnan(dphim));
+plot(s,factor.*dphim./phiedge,'ok');
 pp=polyfit([0 s 1],[0 dphim 0], 6);
 h = 0.01;
 s2 = 0:h:1;
-plot(s2,polyval(pp,s2)./phiedge,'g');
+plot(s2,factor.*polyval(pp,s2)./phiedge,'g');
 legend('Root1','Root2','Avg','Polyfit');
 subplot(1,2,2);
-plot(s,cumsum(dphi1),'b');
+plot(s,factor.*cumsum(dphi1),'b');
 hold on;
-plot(s,cumsum(dphi2),'r');
-plot(s2,cumsum(polyval(pp,s2)),'ok');
+plot(s,factor.*cumsum(dphi2),'r');
+plot(s2,factor.*cumsum(polyval(pp,s2)),'ok');
 xlim([0 1]);
 set(gca,'FontSize',24);
 xlabel('Norm. Toridal Flux (s)');
