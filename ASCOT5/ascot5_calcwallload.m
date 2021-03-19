d454f5cb9467f03a894c6cb1b40c9b1ce4ab4e48
R087104\yxxx <22814736+TheYxxy@users.noreply.github.com>
2021-03-19 18:00:47 +0100

    Remove superfluous

--- ASCOT5/ascot5_calcwallload.m
+++ ASCOT5/ascot5_calcwallload.m
@@ -21,10 +21,6 @@ function wall_load = ascot5_calcwallload(a5file,wallid,runid,varargin)
 %   Version:       1.0  
 
 amu = 1.66053906660E-27;
-<<<<<<< HEAD
-=======
-
->>>>>>> c770c2b4c0f132f4d340294c621a9baf15bf2e28
 wall_load = [];
 pts_mask=[];
 lhits = 0;
@@ -82,12 +78,11 @@ catch
     disp(['ERROR: Could not result: ' num2str(runid,'%10.10i')]);
     return;
 end
-<<<<<<< HEAD
-walltile = h5read(a5file,[path_run '/walltile']); % now in matlab index
-=======
 
->>>>>>> c770c2b4c0f132f4d340294c621a9baf15bf2e28
+
 %walltile = h5read(a5file,[path_run '/walltile'])+1; % now in matlab index
+walltile = h5read(a5file,[path_run '/walltile']); % Old index (better?)
+
 
 % Handle downselect of particles
 if ~isempty(pts_mask)
