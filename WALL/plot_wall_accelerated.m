2f92fdc13bfe31b95dea9b25643ac02fc8756d8f
Dion Engels <d.j.engels@student.tue.nl>
2021-05-19 10:08:02 +0200

    updates wall generation

--- WALL/plot_wall_accelerated.m
+++ WALL/plot_wall_accelerated.m
@@ -7,29 +7,32 @@ function phandle = plot_wall_accelerated(data,varargin)
 %   Written by:     D.J. Engels (d.j.engels@student.tue.nl))
 %   Version:        1.0
 %   Date:           05/21
-figure;
-if nargin>1
-    phandle = plot_divertor(data,varargin{:});
-else
-    phandle = plot_divertor(data);
-end
-hold on;
-% Plot boxes
-for i=1:data.nblocks
-    xs = [data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_min];
-    ys = [data.blocks(i).y_min, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min];
-    zs = [data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max];
-    plot3(xs, ys, zs)
-end
-title("Mesh split up in uniform grid");
-hold off;
-% histogram of how faces are spread over boxes
-figure;
-histogram([data.blocks.nfaces], data.nblocks)
-title("Spread of nfaces over the blocks generated");
-xlabel("Number of faces in a box");
-ylabel("Occurance");
-% Plot information
+
+%% Figure (disabled due to being slow)
+% figure;
+% if nargin>1
+%     phandle = plot_divertor(data,varargin{:});
+% else
+%     phandle = plot_divertor(data);
+% end
+% hold on;
+% % Plot boxes
+% for i=1:data.nblocks
+%     xs = [data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_min, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_max, data.blocks(i).x_min];
+%     ys = [data.blocks(i).y_min, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_max, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min, data.blocks(i).y_min];
+%     zs = [data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max, data.blocks(i).z_min, data.blocks(i).z_max, data.blocks(i).z_max];
+%     plot3(xs, ys, zs)
+% end
+% title("Mesh split up in uniform grid");
+% hold off;
+% % histogram of how faces are spread over boxes
+% figure;
+% histogram([data.blocks.nfaces], data.nblocks)
+% title("Spread of nfaces over the blocks generated");
+% xlabel("Number of faces in a box");
+% ylabel("Occurance");
+
+%% Plot information
 fprintf("----------- Accelerated mesh information -----------\n");
 fprintf("Minimum number of faces in a block: %7d\n", min([data.blocks.nfaces]));
 fprintf("Mean number of faces in a block: %5.1f\n", mean([data.blocks.nfaces]));
--- WALL/wall_acceleration_main.m
+++ WALL/wall_acceleration_main.m
@@ -1,9 +1,19 @@
+% Main for making an accelerated wall
+% Run by setting the filename at the top and giving it a block size
+
+%   Created by: D.J. Engels (d.j.engels@student.tue.nl)
+%   Version:    1.0
+%   Date:       May 2021
+
 %% initialize
 clear all; close all; addpath('c');
 
 
-filename = '/home/dion/WALL_TEST/a10_tokamak_wall.dat';
-block_size = 0.5;
+filename = '/home/dion/Dropbox/__Internship/Internship_local/W7X_FILES/w7x_divertor_op12b_fullres.dat';
+%filename = '/home/dion/WALL_TEST/a10_tokamak_wall.dat';
+%filename = '/home/dion/Dropbox/__Internship/STELLOPT/BENCHMARKS/FIELDLINES_TEST/NCSX_wall_trimesh.dat';
+
+block_size = 0.1;
 
 wall = read_wall(filename);
 %% plot
@@ -18,18 +28,18 @@ plot_wall_accelerated(wall_accelerated);
 pause(0.0001);
 
 %% check output
-% while 1
-%     happy = input("Are you happy with this mesh. Y/N: ", 's');
-%     if happy == "Y"
-%         fprintf("Nice. Continuing to saving of mesh\n");
-%         break
-%     elseif happy == "N"
-%         fprintf("You can change the settings and try again\n")
-%         return
-%     else
-%         fprintf("Invalid input given\n");
-%     end 
-% end
+while 1
+    happy = input("Are you happy with this mesh. Y/N: ", 's');
+    if happy == "Y"
+        fprintf("Nice. Continuing to saving of mesh\n");
+        break
+    elseif happy == "N"
+        fprintf("You can change the settings and try again\n")
+        return
+    else
+        fprintf("Invalid input given\n");
+    end 
+end
 
 %% Save new mesh
 filename_new = strcat(filename(1:end-4), '_acc', num2str(block_size), 'v4', filename(end-3:end));
