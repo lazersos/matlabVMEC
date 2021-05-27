319737fe1063823d65c5151da2573795e1f066e3
Samuel Lazerson <lazersos@gmail.com>
2021-05-27 10:52:25 +0200

    Added ability to override hitonly logic.

--- BEAMS3D/beams3d_finddex.m
+++ BEAMS3D/beams3d_finddex.m
@@ -29,6 +29,7 @@ dex=[];
 eorbit=0; etherm=1; ewall=2; eshine=3; eport=4;
 llast = 0;
 etarg = eorbit;
+lhitonly=1;
 
 % Handle varargin
 if ~isempty(varargin)
@@ -65,6 +66,8 @@ if ~isempty(varargin)
             case 'port_hit'
                 etarg=eport;
                 llast=1;
+            case 'override_hitonly'
+                lhitonly=0;
         end
         i=i+1;
     end
@@ -75,7 +78,7 @@ end_state=double(beam_data.end_state');
 mask=~(end_state==etarg);
 
 % Handle a hitonly run
-if size(beam_data.R_lines,1) == 3
+if and(size(beam_data.R_lines,1) == 3,lhitonly)
     disp('WARNING:  Possible hit_only run!');
     disp('      dex=1 Before hit');
     disp('      dex=2 Wall hit or last point');
