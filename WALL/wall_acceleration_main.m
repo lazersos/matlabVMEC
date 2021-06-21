dd2be13bdd52b9da89f0c9aac5c98844cb2166d6
Dion Engels <d.j.engels@student.tue.nl>
2021-06-21 10:02:51 +0200

    Update wall_acceleration_main

--- WALL/wall_acceleration_main.m
+++ WALL/wall_acceleration_main.m
@@ -9,22 +9,29 @@
 %% initialize
 close all; addpath('c');
 
-filename = '/home/dion/Dropbox/__Internship/Internship_local/W7X_FILES/w7x_divertor_op12b_fullres_0_025.dat';
-%filename = '/home/dion/WALL_TEST/a10_tokamak_wall_0_25.dat';
+main_dir = split(pwd, '/');
+main_dir = strjoin(main_dir(1:end-1), '/');
+addpath(genpath(main_dir));
+
+clear main_dir
+
+filename = '/home/dion/Dropbox/__Internship/Internship_local/W7X_FILES/w7x_divertor_op12b_fullres_0_04.dat';
+%filename = '/home/dion/WALL_TEST/a10_tokamak_wall.dat';
 %filename = '/home/dion/Dropbox/__Internship/STELLOPT/BENCHMARKS/FIELDLINES_TEST/NCSX_wall_trimesh_0_01.dat';
 
-block_size = 0.1;
+block_size = 0.711283596193895;
 
+%tic;
 wall = read_wall(filename);
 %% plot
-plot_wall(wall);
+plot_wall(wall,'wire');
 pause(0.001);
 
 %% run acceleration
 wall_accelerated = wall_acceleration(wall, block_size);
 
 %% plot result
-plot_wall_accelerated(wall_accelerated);
+plot_wall_accelerated(wall_accelerated, 'wire');
 pause(0.0001);
 
 %% check output
