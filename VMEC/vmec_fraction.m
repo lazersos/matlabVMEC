7d966e3f015f7500f6e02511191df1772dd50d38
Samuel Lazerson <lazersos@gmail.com>
2023-04-27 08:35:13 +0200

    Switched from sum to trapz.

--- VMEC/vmec_fraction.m
+++ VMEC/vmec_fraction.m
@@ -8,34 +8,29 @@ function [fpassing,ftrapped] = vmec_fraction(vmec_data)
 %           Phys. Fluids B1, 1663 (1989). 
 %           https://aip.scitation.org/doi/10.1063/1.858945
 %   Example:
-%       booz_data=read_vmec('wout_test.nc');
-%       [fp,ft] = vmec_fraction(booz_data);
+%       vmec_data=read_vmec('wout_test.nc');
+%       [fp,ft] = vmec_fraction(vmec_data);
 %
 %   Maintained by: Samuel Lazerson (samuel.lazerson@ipp.mpg.de)
 %   Version:    1.00
 
 %Defaults
 fpassing = zeros(1,vmec_data.ns);
-ftrapped = zeros(1,vmec_data.ns);
-
 
 % Setup grid
 nth = max(2.*vmec_data.mpol,64);
 nzt = max(2.*vmec_data.ntor,32);
 nla = 64;
-hth = 2.*pi./(nth-1);
-hzt = 2.*pi./(nzt-1);
-hla = 1./(nla-1);
-theta = 0:hth:2*pi;
-zeta  = 0:hzt:2*pi;
-lambda = 0:hla:1;
+theta = linspace(0,2*pi,nth);
+zeta = linspace(0,2*pi,nzt);
+lambda = linspace(0,1,nla);
 
 
 % Transform fields
 b = abs(cfunct(theta,zeta,vmec_data.bmnc,vmec_data.xm_nyq,vmec_data.xn_nyq./vmec_data.nfp));
 g = -cfunct(theta,zeta,vmec_data.gmnc,vmec_data.xm_nyq,vmec_data.xn_nyq./vmec_data.nfp);
-sumg = sum(g,[2 3])';
-b2avg = sum(b.*b.*g,[2 3])'./sumg;
+sumg = trapz(theta,trapz(zeta,g,3),2);
+b2avg = trapz(theta,trapz(zeta,b.*b.*g,3),2)./sumg;
 bmax=max(b,[],[2 3])';
 avgbobm2 = b2avg./(bmax.*bmax);
 b = b./repmat(bmax',[1 length(theta) length(zeta)]);
@@ -46,11 +41,12 @@ for k=2:vmec_data.ns
     if bmax(k) <= 0, continue; end
     avgbpov = zeros(1,nla);
     for l=1:nla
-        avgbpov(l) = sum(sqrt(abs(1 - lambda(l).*b(k,:,:))).*g(k,:,:),[2 3]);
+        avgbpov(l) = trapz(theta,trapz(zeta,sqrt(abs(1 - lambda(l).*b(k,:,:))).*g(k,:,:),3),2);
     end
-    fpassing(k) = 0.75*avgbobm2(k).*sum(lambda./avgbpov).*hla.*sumg(k);
-    ftrapped(k) = 1-fpassing(k);
+    fpassing(k) = 0.75*avgbobm2(k).*trapz(lambda,lambda./avgbpov).*sumg(k);
 end
+fpassing(1) = 2.*fpassing(2)-fpassing(3);
+ftrapped = 1-fpassing;
 
 return;
 
