a9f231b456b2d267eb3ee5b7626bbe337d2b6c12
David K <david.kulla@ipp.mpg.de>
2023-08-08 14:16:35 +0200

    Added suv2rzp

--- BEAMS3D/beams3d_suv2rzp.m
+++ BEAMS3D/beams3d_suv2rzp.m
@@ -23,16 +23,16 @@ tolerance = 1e-9;
 
 % Begin Newton Method
 residual = 1.0;
-factor = 0.5;
-%     if r_out < 0
-%         r_out = raxis(1) + (raxis(end) - raxis(1)) * 0.75;
-%     end
-data.nphi=nphi;
-data.S_ARR=S_ARR;
-data.raxis=raxis;
-data.zaxis=zaxis;
-[r_out,z_out] = beams3d_magaxis(data);
+factor = 1.0;
 
+% data.nphi=nphi;
+% data.S_ARR=S_ARR;
+% data.raxis=raxis;
+% data.zaxis=zaxis;
+%[r_out,z_out] = beams3d_magaxis(data);
+
+ r_out = ones(size(phiaxis')).*(raxis(1)+(raxis(numel(raxis))-raxis(1))*.75);
+ z_out=zeros(size(phiaxis'));
 
 % PHI does not change
 phi_out = mod(v, max(phiaxis));
@@ -48,60 +48,66 @@ u = mod(u, pi2);
 x0 = s .* cos(u);
 y0 = s .* sin(u);
 
-fnorm = x0.^2 + y0.^2;
-fnorm = min(1 ./ fnorm, fnorm_max);
+%fnorm = x0.^2 + y0.^2;
+%fnorm = min(1 ./ fnorm, fnorm_max);
 n = 1;
 X_ARR=S_ARR.*cos(U_ARR);
 Y_ARR=S_ARR.*sin(U_ARR);
-X=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,X_ARR,'cubic','none');
-Y=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,Y_ARR,'cubic','none');
+X=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,X_ARR,'linear','nearest');
+Y=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,Y_ARR,'linear','nearest');
 
-[dXdR, ~, dXdZ] = gradient(X_ARR,hr,hphi,hz);
-[dYdR, ~, dYdZ] = gradient(Y_ARR,hr,hphi,hz);
+[~, dXdR_ARR, dXdZ_ARR] = gradient(X_ARR,hr,hphi,hz);
+[~,dYdR_ARR, dYdZ_ARR] = gradient(Y_ARR,hr,hphi,hz);
 
-dXdR=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dXdR,'cubic','none');
-dXdZ=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dXdZ,'cubic','none');
-dYdR=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dYdR,'cubic','none');
-dYdZ=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dYdZ,'cubic','none');
-
-
+dXdR=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dXdR_ARR,'linear','nearest');
+dXdZ=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dXdZ_ARR,'linear','nearest');
+dYdR=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dYdR_ARR,'linear','nearest');
+dYdZ=griddedInterpolant(R_ARR,PHI_ARR,Z_ARR,dYdZ_ARR,'linear','nearest');
+%figure
+%hold on
 % Loop (Newton's Method)
-while ( n < max_iterations) %residual > tolerance &&
-    %[~, i] = min(max(R_ARR < r_out, 1));
-    %[~, k] = min(max(Z_ARR < z_out, 1));
-    %xparam = (r_out - raxis(i)) * hri(i);
-    %zparam = (z_out - zaxis(k)) * hzi(k);
-
-    % Evaluate the Splines
-    %fvalx = X(r_out,phi_out,z_out);
-    %fvaly =  Y(r_out,phi_out,z_out);
+while ( max(residual,[],'all') > tolerance && n < max_iterations) %residual > tolerance &&
 
     x_term = x0 - X(r_out,phi_out,z_out);
     y_term = y0 - Y(r_out,phi_out,z_out);
 
     detJ = dXdR(r_out,phi_out,z_out) .* dYdZ(r_out,phi_out,z_out) - dYdR(r_out,phi_out,z_out) .* dXdZ(r_out,phi_out,z_out);
-    detJ = sign(detJ).*max(abs(detJ), 0.0001); % Upper bound for step size as detJ enters in denominator
-    delR = -(-sign(detJ).*x_term .* dYdZ(r_out,phi_out,z_out) + y_term .* dXdZ(r_out,phi_out,z_out)) ./ detJ;
-    delZ = -(sign(detJ).*x_term .* dYdR(r_out,phi_out,z_out) - y_term .* dXdR(r_out,phi_out,z_out)) ./ detJ;
+    detJ = max(abs(detJ), 0.0001); % Upper bound for step size as detJ enters in denominator
+    delR = (x_term .* dYdZ(r_out,phi_out,z_out) - y_term .* dXdZ(r_out,phi_out,z_out)) ./ detJ;
+    delZ = (-x_term .* dYdR(r_out,phi_out,z_out) + y_term .* dXdR(r_out,phi_out,z_out)) ./ detJ;
 
     delR = min(max(delR, -hr(1)), hr(1));
     delZ = min(max(delZ, -hz(1)), hz(1));
 
-    residual = (x_term.^2 + y_term.^2) .* fnorm;
+    residual = (x_term.^2 + y_term.^2);%.* fnorm;
 
-    delR(residual < 0.01) = delR(residual < 0.01) * 0.5;
-    delZ(residual < 0.01) = delZ(residual < 0.01) * 0.5;
+    dex=residual < 0.01;
+    delR(dex) = delR(dex) * 0.5;
+    delZ(dex) = delZ(dex) * 0.5;
 
 
     r_out = max(min(r_out + delR * factor, raxis(end)), raxis(1));
     z_out = max(min(z_out + delZ * factor, zaxis(end)), zaxis(1));
 
     n = n + 1;
+    
+    %scatter3(v(1:5:end),r_out(1:5:end),z_out(1:5:end),10.0,residual(1:5:end),'o');
 
 end
+%xlim([0 0.1])
 
 % If the maximum number of iterations is reached, print a warning.
 if n >= max_iterations
+    disp(['Residual max; ',num2str(max(residual,[],'all'))])
+    figure
+    scatter3(v(:),r_out(:),z_out(:),10.0,residual(:),'o');
+    hold on
+    [x_b3d,y_b3d,z_b3d] = meshgrid(phiaxis,raxis,zaxis);
+    %tmp=permute(S_ARR,[3 1 2]);
+    contourslice(x_b3d,y_b3d,z_b3d,S_ARR,[v(1,1,:)],[],[],linspace(0,1,10),'linear');%,linspace(-1,1,20)
+    contourslice(x_b3d,y_b3d,z_b3d,U_ARR,[v(1,1,:)],[],[],linspace(0,2*pi,10),'linear');
+    caxis([0 1])
+    xlim([0 1.1])
     warning('Maximum number of iterations reached.');
 end
 end
