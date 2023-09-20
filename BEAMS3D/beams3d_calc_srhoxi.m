889cd0867348757e5642d4cc8f7e0dd8a200a21e
David K <david.kulla@ipp.mpg.de>
2023-09-20 12:18:02 +0200

    Fixes and renormalization of calc_srhoxi

--- BEAMS3D/beams3d_calc_srhoxi.m
+++ BEAMS3D/beams3d_calc_srhoxi.m
@@ -11,19 +11,23 @@ end
 if ~isfield(beam_data,'pitch')
 beam_data.pitch=beam_data.vll_lines./sqrt(beam_data.vll_lines.^2+beam_data.vperp.^2);
 end
-
 if ~isfield(beam_data,'birth')
 beam_data.birth = beams3d_calc_depo(beam_data);
 end
+
 [s, ~, dVds] = beams3d_volume(beam_data);
 x=0:1.0/beam_data.ns_prof1:1;
-c=(x(1:end-1)+x(2:end)).*0.5;
-%%
-%ind=3:1000;
+%c=(x(1:end-1)+x(2:end)).*0.5;
+%Match beams3d_profiles here
+nc = 128;
+dc = 1./(nc);
+rhoedges = 0:dc:1;
+c=(rhoedges(1:end-1)+rhoedges(2:end)).*0.5;
+
 dex=beam_data.end_state==0|beam_data.end_state==1; %Only particles that arent promptly lost
 f=sqrt(beam_data.S_lines(ind,dex));
 
-rhoedges=[0 beam_data.rho(1:end-1)+diff(beam_data.rho)/2, 1, 1.5]; %correct rho edges
+%rhoedges=[0 beam_data.rho(1:end-1)+diff(beam_data.rho)/2, 1, 1.5]; %correct rho edges
 pitchedges=linspace(-1.,1.,npitch);
 
 [discrho, ~]=discretize(f,rhoedges);
@@ -35,22 +39,27 @@ tmp=repmat(beam_data.Weight(dex)./numel(ind),1,numel(ind))'; %repeat and normali
 beam=repmat(beam_data.Beam(dex),1,numel(ind))';
 
 srhoxi=accumarray([discrho(:),discpitch(:),beam(:)],tmp(:),[numel(rhoedges), numel(pitchedges)+1, max(beam_data.Beam)]);
-srhoxi=srhoxi(1:end-2,1:end-1,:); %Truncate ends from nan and normalize to density
+srhoxi=srhoxi(1:end-1,1:end-1,:); %Truncate ends from nan and normalize to density
 
-%%
 v=pchip([0 sqrt(s)],[0 dVds.*2.*sqrt(s)],c);
 srhoxi=numel(pitchedges)/2*numel(rhoedges)*srhoxi./v'; %Volume normalization
+tmp1 = trapz(pitchedges,squeeze(sum(srhoxi,3)),2);
+tmp2=sum(beam_data.birth,1);
+tmp2=interp1(beam_data.rho,tmp2,c,'linear','extrap');
+tmp3=tmp2'./tmp1;
+srhoxi=srhoxi./tmp3;
+tmp4 = trapz(pitchedges,squeeze(sum(srhoxi,3)),2);
 beam_data.srhoxi=srhoxi;
 beam_data.V=v;
 beam_data.c=c;
 if lplot
-pixplot(beam_data.rho,pitchedges,squeeze(sum(srhoxi,3)))
+pixplot(c,pitchedges,squeeze(sum(srhoxi,3)))
 colorbar
 figure
-plot(beam_data.rho,trapz(pitchedges,squeeze(sum(srhoxi,3)),2),'DisplayName','Integral from S(rho,xi)')
-%plot(beam_data.rho,sum(birth,2))
 hold on
-plot(beam_data.rho,sum(beam_data.birth,1),'DisplayName','From calc depo')
+plot(c,tmp1,'DisplayName','Integral from S(rho,xi)')
+plot(c,tmp4,'DisplayName','Renorm S(rho,xi)')
+plot(c,tmp2,'--','DisplayName','From calc depo')
 legend()
 end
 end
